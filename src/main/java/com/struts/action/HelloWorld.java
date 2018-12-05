f468c4cbd583a322cfb3dac8bee412f3177bfed6
Ravali <Ravali@DESKTOP-CE3CCQ5>
2018-12-05 11:02:41 +0530

    files implimented

---
+++ src/main/java/com/struts/action/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.struts.action;
+
+public class HelloWorld {private String message;
+
+private String userName;
+
+public HelloWorld() {
+}
+
+public String execute() {
+    setMessage("Hello " + getUserName());
+    return "SUCCESS";
+}
+
+public String getMessage() {
+    return message;
+}
+
+public void setMessage(String message) {
+    this.message = message;
+}
+
+public String getUserName() {
+    return userName;
+}
+
+public void setUserName(String userName) {
+    this.userName = userName;
+}
+
+}
---
+++ src/main/webapp/Index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello World</title>
+</head>
+<body>
+    <s:form action="HelloWorld" >
+        <s:textfield name="userName" label="User Name" />
+        <s:submit />
+    </s:form>
+</body>
+</html>
+
+</body>
+</html>
